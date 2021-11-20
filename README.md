6818a175ac80f6a88d03c184f56e7c0a0a69c091
Soumya Swaraj <43204618+SoumyaSwaraj@users.noreply.github.com>
2021-11-20 22:53:24 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center">5x Machine Learning Apps-in-one</h1>
+
+## Demo
+- App Link : <a href="https://mlx2022.herokuapp.com/" target="_blank">mlx2022s</a>
+
+[![All Projects](Demos/main.png)](https://mlx2022.herokuapp.com/)
+
+## Overview
+This [repository](https://github.com/SoumyaSwaraj/Machine-Learning/) consists of various machine learning projects in which each projects was done as end to end projects which means from Data Collection through feature engineering, feature selecion to Deployment and Maintainance. The whole app was built with Flask framework. You can launch my app by clicking <a href="https://mlx2022.herokuapp.com/" target="_blank">here</a>.
+
+For building machine learning models, I have used scikit-learn alias sklearn.
+
+The model was deployed on Heroku platform which is a platform-as-a-service.
+
+## Projects Done
+Here I have listed all the projects that I have done and strats from latest projects.
+
+| Project No |          Project Name           |  Start Date  |   End Date  |           GitHub          |         Blog          | App Link |
+|:-----------|:--------------------------------|:------------:|:-----------:|--------------------------:|----------------------:|---------:|
+| 5.         | Boston Housing Price Prediction | 10-Jul-2020  | 11-Jul-2020 |[Boston Housing](https://github.com/SoumyaSwaraj/Machine-Learning//tree/master/05.Boston_House_Price_Prediction)|[Boston Housing](https://medium.com/@Ashok_kumar_K/boston-housing-price-prediction-e279074f356c?source=your_stories_page---------------------------)|-|
+| 4.         | Loan Status Prediction | 01-Jul-2020  | 07-Jul-2020 |[Loan Status](https://github.com/SoumyaSwaraj/Machine-Learning//tree/master/02.Loan_Status_Prediction)|[Loan Status](https://medium.com/analytics-vidhya/loan-status-prediction-machine-learning-model-ca703246c7ec?source=your_stories_page---------------------------)|-|
+| 3.         | Iris Species Classification | 27-Jun-2020  | 30-Jun-2020 |[Iris Species](https://github.com/SoumyaSwaraj/Machine-Learning//tree/master/01.Iris_Species_Classification)|[Iris Species](https://medium.com/analytics-vidhya/iris-species-classification-machine-learning-model-8d7fa4e48f81?source=your_stories_page---------------------------)|-|
+| 2.         | Gender Classification | 18-Jan-2020  | 31-Jan-2020 |[Gender](https://github.com/SoumyaSwaraj/Machine-Learning//tree/master/03.Gender_Classifcation)|-|-|
+| 1.         | Weight Prediction | 12-Jan-2020  | 17-Jan-2020 |[Weight](https://github.com/SoumyaSwaraj/Machine-Learning//tree/master/04.Weight_Prediction)|-|-|
+
+
+## Technical Aspects
+Since we can deploy atmost 5 app in Heroku as a free service, I came up with a new idea of deploying all my projects in a single web app and controlling the each projects navigation with Flask and Python.
+
+Some extra-ordinary features that I have included in my app :
+- Visualizing Probability of classifcation in each classification type problems.
+- Added all the details about the projects such as data source, code source, libraries and frameworks used in each project's description.
+- All my end to end projects in a single view.
+`
+## Technologies Used
+
+<a href="https://www.python.org/doc/"><img src="static/images/photos/python.png" alt="Python" height="150" width="150" margin="1.3rem"></a><a href="https://www.anaconda.com/"><img src="static/images/photos/anaconda.png" alt="Anaconda" height="150" width="150" margin="1.3rem"></a><a href="https://flask.palletsprojects.com/"><img src="static/images/photos/flask.png" alt="Flask" height="150" width="150" margin="1.3rem"></a><a href="https://matplotlib.org/3.2.2/contents.html"><img src="static/images/photos/matplotlib.jpg" alt="Matplotlib" height="150" width="250" margin="1.3rem"></a>
+<a href="https://seaborn.pydata.org/"><img src="static/images/photos/seaborn.png" alt="Seaborn" height="150" width="150"></a><a href="https://scikit-learn.org/"><img src="static/images/photos/sklearn.png" alt="scikit-learn" height="150" width="200"></a><a href="https://pandas.pydata.org/docs/"><img src="static/images/photos/pandas.png" alt="Pandas" height="150" width="150"></a><a href="https://numpy.org/doc/"><img src="static/images/photos/numpy.png" alt="NumPy" height="150" width="250"></a><a href="https://gunicorn.org/"><img src="static/images/photos/gunicorn.png" alt="gunicorn" height="150" width="300"></a><a href="https://getbootstrap.com/docs/4.5/getting-started/introduction/"><img src="static/images/photos/bootstrap.png" alt="bootstrap" height="150" width="150"></a>
+
+
---
+++ ml_projects.py
@@ -0,0 +1,231 @@
+# Importing Flask essentials
+from flask import Flask, request, render_template, redirect, url_for
+
+# Importing Pandas for Data Reading
+import pandas as pd
+
+# Importing Numpy for Array Manipulation
+import numpy as np
+
+# Importing own modules
+from py_modules import data_extraction as de
+
+# Import pickle module to load models
+import pickle
+
+# Flask App
+ml_app = Flask(__name__)
+
+# Reading my social links data
+social = pd.read_csv('data/social-media-links.csv')
+
+# Extraction of social link data
+social_data = de.get_social_link_data(social)
+
+# Counting number of Data
+number_of_links = len(social_data[0])
+
+# Root or Home page - About Me
+@ml_app.route('/')
+def all_projects():
+	# Reading ML_Projects data
+	ml_projects = pd.read_csv('data/ML_Projects.csv')
+	# Calculating number of projects
+	number_of_projects = ml_projects.shape[0]
+	# Calculating rows for UI
+	rows = (number_of_projects // 3) + 1
+	# Extracting Machine Learning Projects Data
+	ml_projects_data = de.get_ml_projects_data(ml_projects)
+
+	# Extracting Libraries links
+	libraries = ml_projects_data[9]
+	libraries_used = [[str(library) for library in rows.split('|')] for rows in libraries]
+
+	# Extracting Technologies
+	technologies = ml_projects_data[11]
+	technologies_used = [[str(technology) for technology in rows.split('|')] for rows in technologies]
+
+	# Extracting Frame works
+	frameworks = ml_projects_data[10]
+	frameworks_used = [[str(framework) for framework in rows.split('|')] for rows in frameworks]
+
+	# Extracting Tools / IDE
+	tools = ml_projects_data[12]
+	tools_used = [[str(tool) for tool in rows.split('|')] for rows in tools]
+
+	return render_template('all_projects.html',
+                        social_data=social_data,
+                        n = number_of_links,
+                        ml_projects_data = ml_projects_data,
+                        rows = rows,
+                        libraries_used = libraries_used,
+                        technologies_used = technologies_used,
+                        frameworks_used = frameworks_used,
+                        tools_used = tools_used)
+
+
+# Boston House Price Predictor
+@ml_app.route('/boston_house_price_predictor', methods = ['POST', 'GET'])
+def boston_house_price_predictor():
+
+	if request.method == "POST":
+		# Getting Input Data from UI
+		'LSTAT', 'RM', 'PTRATIO', 'INDUS', 'TAX', 'NOX'
+		lstat = float(request.form['lstat'])
+		rm = float(request.form['rm'])
+		ptratio = float(request.form['ptratio'])
+		indus = float(request.form['indus'])
+		tax = float(request.form['tax'])
+		nox = float(request.form['nox'])
+
+		# Forming an input array
+		input_array = [[lstat, rm, ptratio, indus, tax, nox]]
+
+		# Loading Loan Status Predictor Model
+		boston_house_price_predictor = pickle.load(open('models/boston_house_price_predictor.pkl', 'rb'))
+
+		# Prediction
+		price_predicted = np.round(boston_house_price_predictor.predict(input_array), 2)
+
+		# Predicting Probability
+		# predict_proba = boston_house_price_predictor.predict_proba(input_array) * 100
+		return render_template('boston_house_price_predictor.html',
+        	                social_data = social_data,
+            	            n = number_of_links,
+                	        price_predicted = price_predicted,
+                	        input_array = input_array)
+	return render_template('boston_house_price_predictor.html',
+                        social_data = social_data,
+                        n = number_of_links)
+
+
+
+# Loan Status Predictor
+@ml_app.route('/loan_status_predictor', methods = ['POST', 'GET'])
+def loan_status_predictor():
+
+	if request.method == "POST":
+		# Getting Input Data from UI
+		credit_history = float(request.form['credit_history'])
+		loan_amount = float(request.form['loan_amount'])
+		applicant_income = float(request.form['applicant_income'])
+		coapplicant_income = float(request.form['coapplicant_income'])
+		dependents = float(request.form['dependents'])
+
+		# Forming an input array
+		input_array = [[credit_history, loan_amount, applicant_income, coapplicant_income, dependents]]
+
+		# Loading Loan Status Predictor Model
+		loan_status_predictor_model = pickle.load(open('models/loan_status_predictor.pkl', 'rb'))
+
+		# Prediction
+		status_predicted = loan_status_predictor_model.predict(input_array)
+
+		# Predicting Probability
+		predict_proba = loan_status_predictor_model.predict_proba(input_array) * 100
+		return render_template('loan_status_predictor.html',
+        	                social_data = social_data,
+            	            n = number_of_links,
+                	        status_predicted = status_predicted,
+                	        predict_proba = predict_proba)
+	return render_template('loan_status_predictor.html',
+                        social_data = social_data,
+                        n = number_of_links)
+
+
+
+# Iris Species Classifier
+@ml_app.route('/iris_species_classifier', methods = ['POST', 'GET'])
+def iris_species_classifier():
+	if request.method == 'POST':
+		# Getting Data from UI
+		petal_length = float(request.form['petal_length'])
+		petal_width = float(request.form['petal_width'])
+
+		# Forming an Input Array
+		input_array = [[petal_length, petal_width]]
+
+		# Loading Iris Species Classifier Model
+		iris_species_classifier_model = pickle.load(open('models/iris_species_classifier.pkl', 'rb'))
+
+		# Prediction
+		iris_predicted = iris_species_classifier_model.predict(input_array)
+
+		# Predicting Probaility
+		predict_proba = iris_species_classifier_model.predict_proba(input_array) * 100
+
+		return render_template('iris_species_classifier.html',
+        	                social_data = social_data,
+            	            n = number_of_links,
+            	            iris_predicted = iris_predicted,
+            	            predict_proba = predict_proba)
+	return render_template('iris_species_classifier.html',
+        	                social_data = social_data,
+            	            n = number_of_links)
+
+
+
+# Gender Classifier
+@ml_app.route('/gender_classifier', methods = ['POST', 'GET'])
+def gender_classifier():
+	if request.method == 'POST':
+		# Getting Data from UI
+		height = float(request.form['height'])
+		weight = float(request.form['weight'])
+
+		# Forming an Input Array
+		input_array = [[height, weight]]
+
+		# Loading Gender Classifier Model
+		gender_classifier_model = pickle.load(open('models/gender_classifier.pkl', 'rb'))
+
+		# Prediction
+		gender_predicted = gender_classifier_model.predict(input_array)
+
+		# Predicting Probaility
+		predict_proba = gender_classifier_model.predict_proba(input_array) * 100
+
+		return render_template('gender_classifier.html',
+        	                social_data = social_data,
+            	            n = number_of_links,
+            	            gender_predicted = gender_predicted,
+            	            predict_proba = predict_proba)
+	return render_template('gender_classifier.html',
+        	                social_data = social_data,
+            	            n = number_of_links)
+
+
+
+# Weight Predictor
+@ml_app.route('/weight_predictor', methods = ['POST', 'GET'])
+def weight_predictor():
+	if request.method == 'POST':
+		# Getting Data from UI
+		gender = float(request.form['gender'])
+		height = float(request.form['height'])
+
+		# Forming an Input Array
+		input_array = [[height, gender]]
+
+		# Loading Weight Predictor Model
+		weight_predictor_model = pickle.load(open('models/weight_predictor.pkl', 'rb'))
+
+		# Prediction
+		weight_predicted = round(weight_predictor_model.predict(input_array)[0], 2)
+
+		# Predicting Probaility
+		# predict_proba = weight_predictor_model.predict_proba(input_array) * 100
+
+		return render_template('weight_predictor.html',
+        	                social_data = social_data,
+            	            n = number_of_links,
+            	            weight_predicted = weight_predicted,
+            	            input_array = input_array)
+	return render_template('weight_predictor.html',
+        	                social_data = social_data,
+            	            n = number_of_links)
+
+
+# App Launcher
+if __name__ == '__main__':
+	ml_app.run(debug = True, port = 3000)
